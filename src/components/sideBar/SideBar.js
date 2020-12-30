0430d8b6f631a60722bf0bb0f98539f0588f6d1c
Omar Flores Grimontt <dev@omarefg.com>
2020-12-30 12:19:59 -0500

    Added sidebar folder

---
+++ src/components/sideBar/SideBar.js
@@ -0,0 +1,36 @@
+import { Component } from '../../decorators';
+import { Accordion } from '../accordion/Accordion';
+import { HomeLink } from '../homeLink/HomeLink';
+import styles from './Sidebar.module.scss';
+
+function View() {
+  const { routes, router } = this.props;
+
+  const firstLevel = routes.filter(({ father }) => father !== null && father === 0);
+  const children = routes.filter(({ father }) => father !== null);
+
+  const container = document.createElement('nav');
+  container.classList.add(styles.container);
+  container.appendChild(HomeLink({ router }));
+
+  firstLevel.forEach(({ title, id }) => {
+    if (children.filter(({ father }) => father === id).length) {
+      container.appendChild(Accordion({
+        title,
+        onChildClick: (path) => router.goTo({ url: path() }),
+        children: children.filter(({ father }) => father === id),
+      }));
+    }
+    // @TODO create list item for first level without children :)
+  });
+
+  return container;
+}
+
+export const SideBar = (props) => new Component(View, {
+  props,
+  defaultProps: {
+    routes: [],
+    router: null,
+  },
+});
